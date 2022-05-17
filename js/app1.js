71bef08a405d5f89ebf7bed7ba8b430e29fea4db
Meison <89606138203@mail.ru>
2022-05-17 04:03:59 +0300

    Сломалось выведение элементов

--- js/app1.js
+++ js/app1.js
@@ -9,6 +9,7 @@ const $keyboard = $('.keyboardShortcut');
 const $body = $('body');
 const $keyboardImg = $('.keyboard');
 const $shadowInp = $(`<input type="text" class="shadow-inp" name="searchField" style="position: sticky; width: 77px;">`);
+const masColor = ['#CCC8F7', '#DCECFF', '#FFF3B4', '#EBFFB5'];
 const masDropMenu = {
     Сделка: {
         'Сделка|Ответственный': '{{lead.responsible.name}}',
@@ -32,6 +33,7 @@ const masDropMenu = {
 }
 
 
+let lastActiveNode;
 
 function pasteHtmlAtCaret(html) {
     let sel, range;
@@ -59,8 +61,6 @@ function pasteHtmlAtCaret(html) {
                 sel.removeAllRanges();
                 sel.addRange(range);
             }
-
-            console.log(frag, node, lastNode);
         }
     } else if (document.selection && document.selection.type != "Control") {
         // IE < 9
@@ -78,6 +78,28 @@ function modifyInputs() {
     });
 }
 
+function liveSearch() {
+    try {
+        $textInsert.on('keyup', function (e) {
+            let value = $('.shadow-inp')[0].value;
+            let list = $('.innerElem');
+            if (value != '') {
+                list.each(function (par, elem) {
+                    if ($(elem).text().toLowerCase().search(value) == -1) {
+                        $(elem).addClass('hide');
+                    } else {
+                        $(elem).removeClass('hide');
+                    }
+                });
+            } else {
+                list.each(function (par, elem) {
+                    $(elem).removeClass('hide');
+                });
+            }
+        });
+    } catch (error) {}
+}
+
 function addToDiv(event) {
     const emoji = $(event.target).text();
     const dataId = $(event.target).attr('data-id');
@@ -86,20 +108,24 @@ function addToDiv(event) {
     pasteHtmlAtCaret(`<input disabled type="text" class="li-elem" data-id="${dataId}" value='${emoji}'></input>`);
 }
 
-function generateEmojiIcon(emoji, key) {
+function generateEmojiIcon(emoji) {
     const input = document.createElement('input');
+    let liElem = document.createElement('li');
+    liElem.classList.add('inp-wrapper');
+    liElem.append(input);
     input.type = "button";
     input.value = emoji;
     input.innerText = emoji;
     input.classList.add('innerElem');
     input.addEventListener("click", addToDiv);
-    return input;
+    return liElem;
 }
 
 $body.on('keydown', function (e) {
     if (e.key === 'Escape') {
         $('.dropMenu').addClass('active');
         $('.keyboardShortcut').removeClass('keyboardShortcut-active');
+        $('.shadow-inp').remove();
     }
 })
 
@@ -110,14 +136,49 @@ $textInsert.on('keydown', function (e) {
     if (e.key === '[') {
         e.preventDefault();
         $('.dropMenu').removeClass('active');
+
+        let sel, range;
+        if (window.getSelection) {
+            // IE9 and non-IE
+            sel = window.getSelection();
+            if (sel.getRangeAt && sel.rangeCount) {
+                range = sel.getRangeAt(0);
+                range.deleteContents();
+
+                const el = document.createElement("div");
+                el.classList.add('shadow-inp');
+                el.type = "text";
+                el.innerHTML = `<input type="text" class="shadow-inp" name="searchField">`
+                let frag = document.createDocumentFragment(),
+                    node,
+                    lastNode;
+                while ((node = el.firstChild)) {
+                    lastNode = frag.appendChild(node);
+                }
+                range.insertNode(frag);
+
+                if (lastNode) {
+                    range = range.cloneRange();
+                    range.setStartAfter(lastNode);
+                    range.collapse(true);
+                    sel.removeAllRanges();
+                    sel.addRange(range);
+                    liveSearch();
+                    // el.focus();
+                }
+            }
+        } else if (document.selection && document.selection.type != "Control") {
+            // IE < 9
+            document.selection.createRange().pasteHTML(html);
+        }
     }
+    $('.shadow-inp').focus();
 });
 
 $dropMenu.on('click', '.innerElem', function (e) {
     $dropMenu.addClass('active');
-    setTimeout(() => {
-        modifyInputs();
-    }, 0);
+    $('.shadow-inp').remove();
+    modifyInputs(); //Не работает!!
 });
 
 $keyboardImg.on('click', function () {
@@ -135,10 +196,27 @@ $(document).on('click', function (e) {
 });
 
 for (const key in masDropMenu) {
+    $('.dropMenu-nav').append(generateHeaderTitle(key));
     for (const value in masDropMenu[key]) {
         const val = value.replace('|', ' → ');
-        $('.dropMenu-nav').append(generateEmojiIcon(val, key));
+
+        $('.dropMenu-nav').append(generateEmojiIcon(val));
     }
+}
+
+
+
+function generateHeaderTitle(key) {
+    let input = document.createElement('input');
+    let liElem = document.createElement('li');
+    liElem.classList.add('inp-wrapper', 'inp-wrapper-color');
+    liElem.append(input);
+    input.type = "text";
+    input.value = key;
+    input.innerText = key;
+    input.classList.add('dropMenu-elem-color');
+    input.classList.add('innerElem');
+    return liElem;
 }
 
 //Подгон дроп меню к блоку с кареткой
